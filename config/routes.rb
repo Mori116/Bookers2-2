1d94fa6ba3c101e067e7d338e9f1e6eb7b186615
Mori116 <cacaomame096@gmail.com>
2021-08-06 08:23:10 +0000

    [Update]いいね機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :favorites, only: [:create, :destroy]
+  end
 end
