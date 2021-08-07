8317ed7fe6180f5d1e85e47bc35b4df3130dbc14
Mori116 <cacaomame096@gmail.com>
2021-08-07 06:53:31 +0000

    [Add]フォロー/フォロワー機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users, only: [:index, :show, :edit, :update]
+
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
     resources :favorites, only: [:create, :destroy]
   end
+
 end
